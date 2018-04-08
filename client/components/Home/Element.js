2e8436ada14a226df019ecc845b988c28ec3f777
Aric <aric@STL-aric-2.local>
2018-04-08 09:37:26 -0500

    final commits

--- client/components/Home/Element.js
+++ client/components/Home/Element.js
@@ -6,10 +6,12 @@ const Element = props => {
 
   function renderElement() {
     return props.parags.map(pg => {
-      if (parseInt(pg.id) === parseInt(props.id) && pg.field_answers === '') {
+      if (parseInt(pg.id) === parseInt(props.id) && (pg.field_answers === '' && pg.field_text_area === '')) {
         return <FillIn key={pg.id} id={pg.id} title={pg.field_question} />
-      } else if (parseInt(pg.id) === parseInt(props.id) && pg.field_answers !== '') {
+      } else if (parseInt(pg.id) === parseInt(props.id) && (pg.field_answers !== '' && pg.field_text_area === '')) {
         return <Choice key={pg.id} title={pg.field_question} answers={pg.field_answers} parags={props.parags} />
+      } else if (parseInt(pg.id) === parseInt(props.id) && pg.field_text_area !== '') {
+        return <p>{pg.field_text_area}</p>
       }
     })
   }
--- client/components/Home/Home.js
+++ client/components/Home/Home.js
@@ -13,7 +13,9 @@ class Home extends Component {
     super(props)
 
     this.state = {
-      index: 0
+      index: 0,
+      formIndex: 0,
+      finish: false
     }
     
   }
@@ -23,13 +25,13 @@ class Home extends Component {
     this.props.dispatchGetParagraphs();
   }
 
-  renderFormSection(index) {
+  renderFormSection(index,formIndex, finish) {
     if (this.props.state.drupal.forms.length > 0) {
 
-      const sectionIdArr = this.props.state.drupal.forms[0].field_form_elements.split(', ')
+      const sectionIdArr = this.props.state.drupal.forms[formIndex].field_form_elements.split(', ')
       console.log(sectionIdArr)
 
-      if (index >= sectionIdArr.length - 1) {
+      if (index >= sectionIdArr.length - 1 && finish === false) {
         return (
           <div>
             <Section key={sectionIdArr[sectionIdArr.length - 1]} id={sectionIdArr[sectionIdArr.length - 1]} parags={this.props.state.drupal.paragraphs} />
@@ -37,6 +39,16 @@ class Home extends Component {
           </div>
         );
 
+      } else if (finish === true){
+        return (
+          <div class="alert alert-success" role="alert">
+            <h4 class="alert-heading">Well done!</h4>
+            <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+            <hr />
+            <p class="mb-0">Approximate Wait Time: </p>
+            <h1 class="display-4">15 Min</h1>
+          </div>
+        )
       } else {
         return sectionIdArr.map((id, i) => {
           if ( i === index ) {
@@ -75,17 +87,36 @@ class Home extends Component {
     }
   }
 
+  handleFormSelect(i) {
+    this.setState({
+      formIndex: i
+    })
+  }
+
+  renderFormList() {
+    if (this.props.state.drupal.forms.length > 0) {
+      return this.props.state.drupal.forms.map((form, i) => {
+        return <button key={i + 'key'} onClick={() => this.handleFormSelect(i) } className="btn btn-outline-primary">{form.title}</button>
+      })
+    }
+  }
+
   handleFinish() {
-    
+    this.setState({
+      finish: true
+    })
   }
 
   render() {
     console.log(this.props)
     return (
       <div className="container">
+        <div className="form-select">
+          {this.renderFormList()}
+        </div>
 
         {this.renderFormTitle()}
-        {this.renderFormSection(this.state.index)}
+        {this.renderFormSection(this.state.index, this.state.formIndex, this.state.finish)}
         <button onClick={() => this.handleBack() } className="btn btn-primary">Back</button>
         <button onClick={() => this.handleNext() } className="btn btn-primary">Next</button>
       </div>
